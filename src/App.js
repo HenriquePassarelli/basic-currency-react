61be25c105a77f54e51d1c19f0617a7b109b51aa
Henrique Passarelli <henrique.passarellicc@gmail.com>
2021-10-01 10:55:00 -0300

    currency Dollar to Real

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 20,
+      dollar: 60
+    };
+    this.changeValue = this.changeValue.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  changeValue(e) {
+    let value = e.target.value;
+    if (e.target.name.toUpperCase() === 'DOLLAR') {
+      value = value / 3
+    }
+    let dollar = value * 3;
+
+    this.setState({ value, dollar }
+    );
+  }
+
+  render() {
+    return (
+      <div style={{ margin: '1rem' }} >
+        <input type="number" name='real' value={this.state.value} onChange={this.changeValue} /> <br />
+        <input type="number" style={{ marginTop: '1rem' }} name='dollar' value={(this.state.dollar === 0) ? '' : this.state.dollar} onChange={this.changeValue} />
+        <p>Dollar = R$ 3,00 </p>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
